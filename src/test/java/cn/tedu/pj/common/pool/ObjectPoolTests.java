06172a5f4a6ab9fbf4ea39053618f2571b62558b
余浩 <980187223@qq.com>
2020-06-29 17:38:38 +0800

    生命周期

--- src/test/java/cn/tedu/pj/common/pool/ObjectPoolTests.java
+++ src/test/java/cn/tedu/pj/common/pool/ObjectPoolTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @SpringBootTest
 public class ObjectPoolTests {
     @Autowired
@@ -14,5 +17,13 @@ public class ObjectPoolTests {
     @Test
     void testObjectTest(){
         System.out.println(objectPool01==objectPool02);
+    }
+    @PostConstruct//用来初始化生命周期
+    public void init(){
+        System.out.println("init()");
+    }
+    @PreDestroy//用来销毁生命周期
+    public void destroy(){
+        System.out.println("destroy()");
     }
 }
